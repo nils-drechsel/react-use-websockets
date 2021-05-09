6516ee08a34b92a0ebb2783c663434dc47277f89
Nils Drechsel <nils.drechsel@gmail.com>
2021-05-09 19:34:49 +0100

    improving provider

--- src/lib/client/WebSocketProvider.tsx
+++ src/lib/client/WebSocketProvider.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect, useRef, useState, cloneElement, ReactElement, useContext } from "react";
+import React, { FunctionComponent, useEffect, useState, cloneElement, ReactElement, useContext } from "react";
 import { WebSocketManager } from "./WebSocketManager";
 import { WebSocketContext } from "./WebSocketContext";
 import { ErrorBoundary, FallbackProps } from "react-error-boundary";
@@ -15,42 +15,47 @@ interface Props {
     ping?: number;
 }
 
-
-export const WebSocketProvider: FunctionComponent<Props> = ({ id, url, domain, delimiter, ping, logging, reconnect, showElementWhileConnecting, children }) => {
-
+export const WebSocketProvider: FunctionComponent<Props> = ({
+    id,
+    url,
+    domain,
+    delimiter,
+    ping,
+    logging,
+    reconnect,
+    showElementWhileConnecting,
+    children,
+}) => {
     const managerMap: Map<string, WebSocketManager> = useContext(WebSocketContext);
 
-    const managerRef = useRef<WebSocketManager>();
-
-    if (!managerRef.current) {
-        managerRef.current = new WebSocketManager(url, domain, delimiter || "\t", reconnect, ping, logging || false);
-    }
+    const [manager] = useState<WebSocketManager>(
+        new WebSocketManager(url, domain, delimiter || "\t", reconnect, ping, logging || false)
+    );
 
     useEffect(() => {
         return () => {
-            if (managerRef.current) managerRef.current.destroy();
-        }
-    }, []);
+            manager.destroy();
+        };
+    }, [manager]);
 
-    const [, setUid] = useState(null as string | null);
+    const [, setUid] = useState(null as string | null);
 
-  
     const onError = (error: any, info: any) => {
         const errorBean: ClientErrorBean = {
             message: "" + error,
-            componentStack: info?.componentStack
-        }
-        managerRef.current!.send(CoreMessage.CLIENT_ERROR, errorBean);
+            componentStack: info?.componentStack,
+        };
+        if (manager) manager.send(CoreMessage.CLIENT_ERROR, errorBean);
     };
 
     const errorFallback = ({}: FallbackProps) => {
         return <div>An error has occurred and was registered</div>;
-    };    
+    };
 
     const [isSocketConnected, setSocketConnected] = useState(false);
-    
+
     useEffect(() => {
-        const unsubscribe = managerRef.current!.addConnectivityListener((_isConnected, isReady, _sid, uid) => {
+        const unsubscribe = manager.addConnectivityListener((_isConnected, isReady, _sid, uid) => {
             if (logging) console.log("setting connectivity for uid:", uid, "ready:", isReady);
             setSocketConnected(isReady);
             setUid(uid);
@@ -58,25 +63,24 @@ export const WebSocketProvider: FunctionComponent<Props> = ({ id, url, domain, d
 
         return () => {
             unsubscribe();
-        }
-
+        };
     }, []);
 
-    if (!isSocketConnected && showElementWhileConnecting) {
-        return (<div>{cloneElement(showElementWhileConnecting)}</div>);
+    if ((!manager || !isSocketConnected) && showElementWhileConnecting) {
+        return <div>{cloneElement(showElementWhileConnecting)}</div>;
     }
 
     const map = new Map();
     if (managerMap) {
         managerMap.forEach((value, key) => map.set(key, value));
     }
-    map.set(id, managerRef.current);
+    map.set(id, manager);
 
     return (
         <WebSocketContext.Provider value={map}>
             <ErrorBoundary onError={onError} fallbackRender={errorFallback}>
-                { children }
+                {children}
             </ErrorBoundary>
         </WebSocketContext.Provider>
-    )
-}
+    );
+};
